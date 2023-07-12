068898dead1ce643d8d36521c38579e3d6a4c832
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-07-12 04:08:24 +0300

    add `Flask CORS`, change get `nl_query` to `query`

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
+import logging
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from translator import process_nl_query
 
+logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/api/query', methods=['GET'])
+@app.route("/api/query", methods=["POST"])
 def api_query():
     """
     API endpoint to process Natural Language Query
     """
     data = request.get_json()
-    nl_query = data.get('nl_query')
+    nl_query = data.get("query")
+    logging.info(f"Received NL query: {nl_query}")
 
     sql_statements, results = process_nl_query(nl_query)
 
-    return jsonify(
-        sql_outputs=[{'sql': s, 'result': r}
-                     for s, r in zip(sql_statements, results)]
-    )
+    return jsonify([{"sql": s, "result": r} for s, r in zip(sql_statements, results)])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
