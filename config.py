09d6e2fa0654100d60d8d762df2e13f42b7379c1
OBADA TAHAYNA <obada.jaras@yahoo.com>
2023-07-14 05:27:09 +0300

    make is 2 phases of generations

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ DATABASE_CONFIG = {
 }
 
 MODEL_CONFIG = {
-    'model_name': 'obada-jaras/PANL_SQL',
-    'number_of_outputs': 3
+    'model_name': 'obada-jaras/PANL_SQL_v0.2',
+    'number_of_outputs': 3,
+    'max_length': 512,
+    'top_k': 120,
+    'top_p': 0.95
 }
--- translator.py
+++ translator.py
@@ -1,33 +1,47 @@
-import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from config import MODEL_CONFIG
 from database import execute_and_fetch_query
-import logging
 
 tokenizer = T5Tokenizer.from_pretrained(MODEL_CONFIG['model_name'])
 model = T5ForConditionalGeneration.from_pretrained(MODEL_CONFIG['model_name'])
 
 
-def generate_sql_statements(nl_input: str, num_outputs: int) -> list:
-    """ Generate multiple SQL statements from the input natural language query."""
+def generate_sql_statements(nl_input: str) -> list:
+    """ Generate the most probable SQL statement and two diverse statements 
+    from the input natural language query."""
     encoding = tokenizer.encode_plus(
-        nl_input, padding='max_length', max_length=512, return_tensors="pt")
+        nl_input, padding='max_length', max_length=MODEL_CONFIG['max_length'], return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"], encoding["attention_mask"]
 
     sql_statements = []
-    for id in range(num_outputs):
-        torch.manual_seed(id+1)
-        outputs = model.generate(
-            input_ids=input_ids, attention_mask=attention_masks,
-            max_length=512,
-            do_sample=True,
-            top_k=120,
-            top_p=0.95,
-            early_stopping=True,
-            num_return_sequences=1
-        )
+
+    # Generate the most probable output
+    most_probable_output = model.generate(
+        input_ids=input_ids,
+        attention_mask=attention_masks,
+        max_length=MODEL_CONFIG['max_length'],
+        do_sample=False,
+        early_stopping=True,
+        num_return_sequences=1
+    )
+    sql = tokenizer.decode(
+        most_probable_output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    sql_statements.append(sql)
+
+    # Generate two more diverse outputs
+    diverse_outputs = model.generate(
+        input_ids=input_ids,
+        attention_mask=attention_masks,
+        max_length=MODEL_CONFIG['max_length'],
+        do_sample=True,
+        top_k=MODEL_CONFIG['top_k'],
+        top_p=MODEL_CONFIG['top_p'],
+        early_stopping=True,
+        num_return_sequences=MODEL_CONFIG['number_of_outputs']-1
+    )
+    for output in diverse_outputs:
         sql = tokenizer.decode(
-            outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+            output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
         sql_statements.append(sql)
 
     return sql_statements
@@ -37,10 +51,8 @@ def process_nl_query(nl_query: str):
     """
     Translates Natural Language query to SQL, executes it and fetches the results.
     """
-    logging.info(f"Received NL query: {nl_query}")
 
-    sql_statements = generate_sql_statements(
-        nl_query, MODEL_CONFIG['number_of_outputs'])
+    sql_statements = generate_sql_statements(nl_query)
     results = [execute_and_fetch_query(statement)
                for statement in sql_statements]
 
